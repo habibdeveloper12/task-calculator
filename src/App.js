290db47e2577b9ee4ab82dfc0650514c5fe33e27
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-11-20 23:55:46 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import About from './components/About';
-import AllInOne from './components/AllInOne';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar'
-import Pricing from './components/Pricing';
-import Support from './components/Support'
+import AllInOne from "./components/AllInOne";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
+      <Calculator />
       <Support />
       <AllInOne />
       <Pricing />
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useForm } from "react-cool-form";
+
+import "./../styles.css";
+const Field = ({ label, id, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <input id={id} {...rest} />
+  </div>
+);
+
+const Select = ({ label, id, children, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <select id={id} {...rest}>
+      {children}
+    </select>
+  </div>
+);
+
+const Textarea = ({ label, id, ...rest }) => (
+  <div>
+    <label htmlFor={id}>{label}</label>
+    <textarea id={id} {...rest} />
+  </div>
+);
+const Calculator = () => {
+  const [financialHealth, setFinancialHealth] = useState(null);
+  const { form } = useForm({
+    defaultValues: { income: 0, expenses: 0, debts: 0, assets: 0 },
+    onSubmit: async (values) => {
+      const response = await fetch(
+        "http://localhost:3001/calculateFinancialHealth",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        }
+      );
+
+      const data = await response.json();
+      setFinancialHealth(data.financialHealth);
+    },
+  });
+
+  return (
+    <div name="about" className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto">
+        <div className="text-center">
+          <h2 className="text-5xl font-bold">
+            Trusted by developers across the world
+          </h2>
+          <p className="text-3xl py-6 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque
+            asperiores earum placeat veritatis dignissimos itaque.
+          </p>
+        </div>
+
+        <div className="grid md:grid-cols-1 gap-1 px-2 text-center">
+          <div className="border py-8 rounded-xl shadow-xl">
+            <form ref={form}>
+              <Field
+                label="monthly income,"
+                id="income"
+                name="income"
+                type="number"
+              />
+              <Field
+                label="monthly expenses,"
+                id="expenses"
+                name="expenses"
+                type="number"
+              />
+              <Field
+                label="monthly debts,,"
+                id="debts"
+                name="debts"
+                type="number"
+              />
+              <Field
+                label="monthly assets.,"
+                id="assets"
+                name="assets"
+                type="number"
+              />
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
+        {financialHealth !== null && <p>Financial Health: {financialHealth}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
